6090be32c8bbf59bd253439bd6e215013feac637
Garry Rudman <garryrudman@users.noreply.github.com>
2020-04-15 15:43:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+let firstName; // definition
+let lastName; // definition
+let age = 65; // declaration and defination
+let lightOn = true; // single = is assignment, double == is check
+
+function lightSwitch() {
+
+if (lightOn == true) { // condition one
+    lightOn = false 
+    console. log(lightOn) // logs to the console the state of lightOn
+}
+else { // condition two
+    lightOn = true
+    console. log(lightOn) // logs to the console the state of lightOn
+    }
+}
+
+lightSwitch()
+lightSwitch()
+lightSwitch()
+let numberOne;
+let numberTwo;
+let total;
+
+function addition() {
+    total = numberOne + numberTwo
+    console. log(total)
+}
+
+addition(20, 30) //50
+addition(123, 56) // 179
+addition(44, 45) // 89
+
+
+// declaration & definition
+// variable: storage box.
+// firstName: Garry
+// LastName: Rudman
+// age: 65
